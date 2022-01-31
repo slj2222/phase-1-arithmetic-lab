9798ad2e90493e123f682b92fd99e96f50df3e4b
Spencer Johnson <sljohnson2222@gmail.com>
2022-01-31 17:31:12 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const num1 = 2;
+const num2 = 31;
 
-// Write your code here
+const multiply = num1 * num2;
+
+
+
+const random = Math.ceil(Math.random() *10)
+
+
+
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+
+
+const max = Math.max(1 , 2, 9, 20);
